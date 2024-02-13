f440c1ee248e14c182b0ff5036f5fd2776733ce8
CallMePeteio <pfalzerpetter@gmail.com>
2024-02-13 20:12:50 +0100

    added ffmpeg logging option and capped fps

--- convertToRtmp.py
+++ convertToRtmp.py
@@ -9,7 +9,7 @@ def start_TCP(url, res, fps):
     videoCommand = f'rpicam-vid -t 0 --libav-format mpegts --width {res[0]} --height {res[1]} --framerate {fps} --listen -o "{url}?listen=1"'
     process = subprocess.run(videoCommand, shell=True, check=True) # RUNS THE COMMAND
 
-def tcp_to_rtmp(tcp_url, rtmp_url, rtmp_key):
+def tcp_to_rtmp(tcp_url, rtmp_url, rtmp_key, log, fps=30):
     """
     Converts a TCP stream to an RTMP stream using FFmpeg.
 
@@ -24,13 +24,17 @@ def tcp_to_rtmp(tcp_url, rtmp_url, rtmp_key):
     command = [
         'ffmpeg',
         '-i', tcp_url,  # Input from the TCP stream
+        '-r', str(fps),  # Specify the frame rate for the output video
         '-c', 'copy',   # Copy the stream without re-encoding
         '-f', 'flv',    # Format for the RTMP stream
-        full_rtmp_url   # Output RTMP URL
+        full_rtmp_url,   # Output RTMP URL
     ]
 
-    err = subprocess.run(command, stderr=subprocess.PIPE, text=True)  # Execute the FFmpeg command
+    if log == True:
+        command.append("-report") # FFMPEG WIL GENNERATE A LOG FILE UPON START, BETTER FOR DEBUGGING
 
+    err = subprocess.run(command, stderr=subprocess.PIPE, text=True, check=True)  # Execute the FFmpeg command
+    
     if err.returncode != 0: # IF IT WAS UNSUCSESSFULL
         print("\n \n Returnd an error")
         print(f"{err.stderr} \n \n")
@@ -40,8 +44,9 @@ def tcp_to_rtmp(tcp_url, rtmp_url, rtmp_key):
 with open('./config.json') as f:
   streamSettings = json.load(f)
 
+
 threading.Thread(target=start_TCP, args=(streamSettings["tcpUrl"], streamSettings["res"], streamSettings["fps"])).start() # RUNS THE TCP STREAM IN A SEPERATE THREAD
 time.sleep(3) # WAITS FOR THE TCP STREAM TO START
-tcp_to_rtmp(streamSettings["tcpUrl"], streamSettings["rtmpUrl"], streamSettings["rtmpKey"]) # STARTS STREAMING TO THE SERVER
+tcp_to_rtmp(streamSettings["tcpUrl"], streamSettings["rtmpUrl"], streamSettings["rtmpKey"], streamSettings["log-ffmpeg"], streamSettings["fps"]) # STARTS STREAMING TO THE SERVER
 
 
